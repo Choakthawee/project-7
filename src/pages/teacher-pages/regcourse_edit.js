d4891a4f8cd124a89c899b0c36f747a449883b94
kanomkway <jenny.yatika@gmail.com>
2024-02-21 13:37:24 +0700

    regcourse

--- src/pages/teacher-pages/regcourse_edit.js
+++ src/pages/teacher-pages/regcourse_edit.js
@@ -1,35 +1,38 @@
 //ใส่ข้อมูลลงทะเบียนรายวิชา (อาจารย์)
 import { useNavigate } from "react-router";
 import Swal from "sweetalert2";
+import React, { useState } from "react";
+import "./reg-set.css";
+
 const RegCourseEdit = () => {
   const userRole = localStorage.getItem("role");
   const navigate = useNavigate();
 
   const showAlert = () => {
     Swal.fire({
-      icon: 'error',
-      title: 'ข้อผิดพลาด',
-      text: 'คุณไม่มีสิทธิ์เข้าถึงหน้านี้',
-      confirmButtonColor: '#3085d6',
-      confirmButtonText: 'ตกลง'
+      icon: "error",
+      title: "ข้อผิดพลาด",
+      text: "คุณไม่มีสิทธิ์เข้าถึงหน้านี้",
+      confirmButtonColor: "#3085d6",
+      confirmButtonText: "ตกลง",
     }).then((result) => {
       if (result.isConfirmed) {
         if (userRole === "admin") {
-          navigate('/userinfo');
+          navigate("/userinfo");
         } else if (userRole === "education department") {
-          navigate('/imcourse');
+          navigate("/imcourse");
         }
       }
     });
   };
 
-  if (userRole !== 'teacher') {
+  if (userRole !== "teacher") {
     showAlert();
     return null;
   }
-  
+
   return (
-    <div>
+    <div className="background">
       <h1>RegCourseEdit</h1>
     </div>
   );
