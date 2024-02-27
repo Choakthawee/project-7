0d2a2a921d0814d730d2ce20c706b64316d58ae4
Buglot <65818720+buglot@users.noreply.github.com>
2024-02-27 16:54:51 +0700

    edit bug responesive

--- src/pages/ed-pages/imcourse.js
+++ src/pages/ed-pages/imcourse.js
@@ -206,7 +206,7 @@ const ImportCourse = () => {
               <div>
                 <p className="text-sm font-medium ">หมวดวิชา</p>
               </div>
-              <div className="flex items-center box-border min-w-40">
+              <div className="flex items-center box-border md:min-w-40">
                 <select
                   className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
                   style={{ height: 40 }}
