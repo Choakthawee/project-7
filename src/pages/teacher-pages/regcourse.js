e8bf4c0fd13c12ad0829226c8060dffd383249fc
Buglot <65818720+buglot@users.noreply.github.com>
2024-03-02 17:57:20 +0700

    fix responsive web and add button page

--- src/pages/teacher-pages/regcourse.js
+++ src/pages/teacher-pages/regcourse.js
@@ -6,6 +6,7 @@ import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { faArrowAltCircleDown } from "@fortawesome/free-solid-svg-icons";
 import { RiEdit2Fill } from "react-icons/ri";
 import React, { useEffect, useState } from "react";
+import { FaCircleLeft, FaCircleRight } from "react-icons/fa6";
 import "./reg-set.css";
 import axios from "axios";
 import { apiurl } from "../../config";
@@ -35,8 +36,8 @@ const RegCourse = () => {
   const [subjects, setSubjects] = useState([]);
   const [noneSubject, setNoneSubject] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const subjectsPage = 7;
-  const totalPages = Math.ceil(subjects.length / subjectsPage);
+  const subjectsPage = 10;
+  const totalPages = subjects.results?Math.ceil(subjects.results.length / subjectsPage):1;
   const startIndex = (currentPage - 1) * subjectsPage;
   const endIndex = startIndex + subjectsPage;
 
@@ -136,24 +137,25 @@ const RegCourse = () => {
     return null;
   }
   return (
-    <div className="flex flex-auto overflow-hidden h-screen background21">
-      <div className="flex-1 flex flex-col p-10 gap-5">
-        <h1 className="flex  font-family font-bold text-4xl size-30 text-midgreen h1text-shadow">
+    <div className="flex flex-auto h-full min-h-screen  background21  w-full">
+      <div className="flex flex-col p-2 md:p-10 gap-5 w-full">
+        <h1 className="flex font-family font-bold text-4xl size-30 text-midgreen h1text-shadow">
           ลงทะเบียนรายวิชา
         </h1>
-        <div className="flex">
-          <div className="flex font-family text-xl font-medium">
-            <p className="flex font-family text-xl font-medium ptext-shadow mr-3 mt-1">
+        <div className="flex gap-2">
+          <div className="flex font-family text-xl font-medium items-center gap-2">
+            <p className="flex font-family text-xl font-medium ptext-shadow">
               ภาคเรียน <span style={{ color: "red" }}>*</span>
             </p>
             <div style={{ position: "relative" }}>
               <select
                 className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
                 style={{ width: 120, height: 40 }}
+                defaultValue={""}
               // value={statusSem}
               // onChange={handleSemStatusChange}
               >
-                <option value="" disabled selected hidden>
+                <option value="" disabled hidden>
                   ---
                 </option>
                 <option>ต้น</option>
@@ -171,23 +173,20 @@ const RegCourse = () => {
               />
             </div>
           </div>
-          <div
-            style={{
-              flex: 4,
-            }}
-            className="flex font-family text-xl font-medium ml-2"
+          <div className="flex font-family text-xl font-medium items-center gap-2"
           >
-            <p className="flex font-family text-xl font-medium ptext-shadow mr-3 mt-1">
+            <p className="flex font-family text-xl font-medium ptext-shadow">
               ปีการศึกษา <span style={{ color: "red" }}>*</span>
             </p>
             <div style={{ position: "relative" }}>
               <select
                 className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
                 style={{ width: 120, height: 40 }}
+                defaultValue={""}
               // value={statusYear}
               // onChange={handleYearStatusChange}
               >
-                <option value="" disabled selected hidden>
+                <option value="" disabled hidden>
                   ---
                 </option>
                 {[...Array(10 + 1).keys()].map((index) => {
@@ -208,46 +207,29 @@ const RegCourse = () => {
             </div>
           </div>
         </div>
-        <div
-          style={{
-            display: "flex",
-          }}
-        >
-          <div
-            style={{
-              flex: 4,
-            }}
-          >
-            <form>
-              <div>
-                <label
-                  for="first_name"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
-                >
-                  วิชา/รหัสวิชา
-                </label>
-                <input
-                  type="text"
-                  id="course_code"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-400 dark:placeholder-gray-200 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="โปรดระบุวิชาหรือรหัสวิชา"
-                  required
-                />
-              </div>
-            </form>
+        <div className="flex gap-2 flex-col md:flex-row">
+          <div className=" flex flex-col gap-2">
+            <label className="block text-sm font-medium text-gray-900 dark:text-black">
+              วิชา/รหัสวิชา
+            </label>
+            <input type="text" id="course_code" className="bg-gray-50 border md:min-w-36 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-400 dark:placeholder-gray-200 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="โปรดระบุวิชาหรือรหัสวิชา"
+              required
+            />
           </div>
-          <div className="flex flex-1 font-family font-medium ml-7 flex-col">
+          <div className="flex font-family font-medium flex-col gap-2">
             <div>
-              <p className="text-sm font-medium mb-2">หลักสูตร</p>
+              <p className="text-sm font-medium ">หลักสูตร</p>
             </div>
             <div style={{ position: "relative" }}>
               <select
-                className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-                style={{ width: 140, height: 40 }}
-              // value={statusSyl}
+                className="block appearance-none w-full  md:w-36 md:h-10 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+
+                // value={statusSyl}
+                defaultValue={""}
               // onChange={handleSylStatusChange}
               >
-                <option value="" disabled selected hidden>
+                <option value="" disabled hidden>
                   ---
                 </option>
                 <option>2566</option>
@@ -269,19 +251,20 @@ const RegCourse = () => {
               />
             </div>
           </div>
-          <div className="flex flex-1 font-family font-medium ml-7 flex-col"
-          >
+          <div className="flex font-family font-medium flex-col">
             <div>
               <p className="text-sm font-medium mb-2">หมวดวิชา</p>
             </div>
             <div style={{ position: "relative" }}>
               <select
-                className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-                style={{ width: 140, height: 40 }}
+                className="block appearance-none w-full  md:w-36 md:h-10 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+
+                defaultValue={""}
+
               // value={statusCat}
               // onChange={handleCatStatusChange}
               >
-                <option value="" disabled selected hidden>
+                <option value="" disabled hidden>
                   ---
                 </option>
                 <option>แกน</option>
@@ -300,34 +283,24 @@ const RegCourse = () => {
               />
             </div>
           </div>
-          <div
-            style={{
-              flex: 6,
-            }}
-            className="font-family mt-7 ml-10"
-          >
+          <div className="font-family flex  items-end ">
             <button
               type="button"
-              class="flex items-center focus:outline-none text-white hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+              className="flex items-center focus:outline-none text-white hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg px-5 py-2.5 mb-1 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
               style={{
                 backgroundColor: "#134e4a",
                 width: 110,
                 height: 35,
-              }}
-            >
+              }} >
               <p className="text-lg mr-2">ค้นหา</p>
-              <FontAwesomeIcon
-                icon={faMagnifyingGlass}
-                className="mr-2"
-                style={{ fontSize: "18px" }}
-              />
+              <FontAwesomeIcon icon={faMagnifyingGlass} style={{ fontSize: "18px" }} />
             </button>
           </div>
         </div>
         {noneSubject.msgerrortime ?
           <div className=" text-2xl text-red-700 text-center underline">{noneSubject.msgerrortime}</div> :
           <div className="flex flex-col gap-2">
-            <h1 className=" h1text-shadow text-xl">{subjects.msgtime &&`*${subjects.msgtime}`}</h1>
+            <h1 className=" h1text-shadow text-xl">{subjects.msgtime && `*${subjects.msgtime}`}</h1>
             <div className="flex flex-1 bg-slate-200 rounded-lg overflow-x-auto shadow-xl">
               <table className="h-full w-full">
                 <thead>
@@ -340,43 +313,44 @@ const RegCourse = () => {
                     <th className="py-2 font-light text-xl">ลงทะเบียน</th>
                   </tr>
                 </thead>
-
                 {currentsubjects.map((v, i) => (
                   <tbody key={startIndex + i}
-
                     className={
                       (startIndex + i) % 2 === 0
                         ? "bg-gray-100"
                         : "bg-white"
                     }
                   >
-                    <td className="py-2 font-light text-lg text-center">{i + 1}</td>
-                    <td className="py-2 font-light text-lg text-center">
-                      {v.idsubject}
-                    </td>
-                    <td className="py-2 font-light text-lg text-center">
-                      {v.name}
-                    </td>
-                    <td className="py-2 font-light text-lg text-center">
-                      {v.years}
-                    </td>
-                    <td className="py-2 font-light text-lg text-center">
-                      {v.subject_category}
-                    </td>
-                    <td className="py-2 font-light text-lg text-center">
-                      <button
-                        className="text-green-950 py-1 px-2 rounded-md font-light"
-                        onClick={() => {
-                          subjects.msgtime ? Swal.fire({
-                            icon: "warning", "text": subjects.msgtime, title: "ข้อผิดพลาด",
-                            confirmButtonColor: "#3085d6",
-                            confirmButtonText: "ตกลง",
-                          }) : handleSwab(v)
-                        }}
-                      >
-                        {subjects.msgtime ? <LockIcon></LockIcon> : <RiEdit2Fill size={20} />}
-                      </button>
-                    </td>
+                    <tr>
+                      <td className="py-2 font-light text-lg text-center">{i + 1}</td>
+                      <td className="py-2 font-light text-lg text-center">
+                        {v.idsubject}
+                      </td>
+                      <td className="py-2 font-light text-lg text-center">
+                        {v.name}
+                      </td>
+                      <td className="py-2 font-light text-lg text-center">
+                        {v.years}
+                      </td>
+                      <td className="py-2 font-light text-lg text-center">
+                        {v.subject_category}
+                      </td>
+                      <td className="py-2 font-light text-lg text-center">
+                        <button
+                          className="text-green-950 py-1 px-2 rounded-md font-light"
+                          onClick={() => {
+                            subjects.msgtime ? Swal.fire({
+                              icon: "warning", "text": subjects.msgtime, title: "ข้อผิดพลาด",
+                              confirmButtonColor: "#3085d6",
+                              confirmButtonText: "ตกลง",
+                            }) : handleSwab(v)
+                          }}
+                        >
+                          {subjects.msgtime ? <LockIcon></LockIcon> : <RiEdit2Fill size={20} />}
+                        </button>
+                      </td>
+                    </tr>
+
                   </tbody>
                 ))}
               </table>
@@ -384,8 +358,21 @@ const RegCourse = () => {
             <h1 className="text-center text-xl">{subjects.msg}{noneSubject.msgerror}</h1>
           </div>
         }
-
+      <div className="flex h-full justify-center items-end">
+        <div className="flex justify-center items-center gap-3">
+          <button onClick={handlePrevPage}>
+            <FaCircleLeft size={21} color="#0a6765" className="" />
+          </button>
+          <p className="text-lg font-semibold text-midgreen">
+            หน้า {currentPage} จาก {totalPages}
+          </p>
+          <button onClick={handleNextPage}>
+            <FaCircleRight size={21} color="#0a6765" className="" />
+          </button>
+        </div>
       </div>
+      </div>
+      
     </div>
   );
 };
