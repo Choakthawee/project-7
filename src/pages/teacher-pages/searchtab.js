31ead537e53279253a419eee10061d9f844315a8
Buglot <65818720+buglot@users.noreply.github.com>
2024-03-02 16:59:39 +0700

    add searchingbar popop

---
+++ src/pages/teacher-pages/searchtab.js
@@ -0,0 +1,32 @@
+export default function SearchTab({ searching, setSearching, loading, handleInputChange }) {
+    function TabSubject({ index, name, idsubject }) {
+        return (
+            <button type='button' key={index}
+                className='flex p-2 justify-start gap-2'
+                value={name} onClick={(e) => { handleInputChange(e); setSearching(null) }} >
+                <a className=" text-gray-400">{idsubject}</a>
+                {name}
+            </button>
+        );
+    }
+    function TabName({ index, name, idsubject }) {
+        return;
+    }
+    return (
+        <div className='mt-16 bg-white w-1/2 max-h-44 absolute flex flex-col overflow-y-auto'>
+            {searching.length > 0 ? (
+                searching.map((v, i) => (
+                    v.name ?
+                        <TabSubject key={i} index={i} name={v.name} year={v.years} idsubject={v.idsubject}></TabSubject>
+                        : <input type='button' key={i} className='' value={v.username} onClick={handleInputChange} />
+                ))
+            ) : (
+                loading ? (
+                    <input type='button' className='' value={"Loading..."} />
+                ) : (
+                    <p>{searching.error}</p>
+                )
+            )}
+        </div>
+    )
+}
