83ebdaa660c919f035fd9f5c08c5078ab69d3d2a
Buglot <65818720+buglot@users.noreply.github.com>
2024-03-07 18:10:52 +0700

    add new table

--- src/pages/admin-pages/table-edit.js
+++ src/pages/admin-pages/table-edit.js
@@ -1,7 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
-
-import Pidbox from "./component/pidbox";
+import BoxSetDB from "./component/Boxdb";
 const TableEdit = () => {
     const navigate = useNavigate();
     const userRole = localStorage.getItem("role_id");
@@ -36,20 +35,46 @@ const TableEdit = () => {
                     ตั้งค่า Database
                 </p>
 
-                <Pidbox title={"PID"}>
-                    <div>
-                        sasdasd
+                <BoxSetDB title={"PID"}>
+                    <div className="flex gap-4 items-center">
+                        <div className="bg-slate-300 p-2 pl-5 pr-5   rounded-lg">
+                            {123123}
+                        </div>
+                        <div>
+                            reload
+                        </div>
                     </div>
-                    <div>
-                        sasdasd
+                </BoxSetDB>
+
+                <BoxSetDB title={"หมวดวิชา"}>
+                    <div className=" rounded-lg overflow-x-auto shadow-xl">
+                        <table className=" w-full">
+                            <thead className="column-color1 text-white ">
+                                <tr>
+                                    <th>id</th>
+                                    <th>หมวด</th>
+                                    <th>แก้ไข</th>
+                                    <th>ลบ</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr className=" bg-slate-100">
+                                    <td className=" text-center p-2  border border-yellow-950">1</td>
+                                    <td className=" text-center p-2 border">วิชาเอก</td>
+                                    <td className=" text-center p-2 border">แก้</td>
+                                    <td className=" text-center p-2 border">ลบ</td>
+                                </tr>
+                                <tr className=" bg-white">
+                                    <td className=" text-center p-2  border">1</td>
+                                    <td className=" text-center p-2 border">วิชาเอก</td>
+                                    <td className=" text-center p-2 border">แก้</td>
+                                    <td className=" text-center p-2 border">ลบ</td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
-                    <div>
-                        sasdasd
-                    </div>
-                    <div>
-                        sasdasd
-                    </div>
-                </Pidbox>
+
+                </BoxSetDB>
             </div>
         </div>
     )
