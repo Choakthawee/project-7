65568cd31b66a10656bc989bb7b6caa5505fb5f9
Buglot <65818720+buglot@users.noreply.github.com>
2024-03-13 03:10:11 +0700

    add selection icon

--- src/pages/admin-pages/component/urlboxicon.js
+++ src/pages/admin-pages/component/urlboxicon.js
@@ -1,6 +1,38 @@
-
-
+import { Settings } from "lucide-react";
+import { FaBars, FaUser, FaClock, FaInfo, FaBook, FaChalkboard, FaList, FaUnlockAlt, FaUsers, FaClipboard, } from "react-icons/fa";
+import MySelect from "./myselect";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { apiurl } from "../../../config";
 export default function UrlboxIcon() {
+    const [data, setData] = useState([])
+    const listicon = [
+        { icon: <FaInfo size={20} /> },
+        { icon: <FaClock size={20} /> },
+        { icon: <Settings size={20} /> },
+        { icon: <FaChalkboard size={20} /> },
+        { icon: <FaBook size={20} /> },
+        { icon: <FaUnlockAlt size={20} /> },
+        { icon: <FaUsers size={20} /> },
+        { icon: <FaClipboard size={20} /> },
+        { icon: <FaList size={20} /> }
+    ]
+    const [reload, setReload] = useState(false)
+    const [Errormsg, setErrormsg] = useState();
+    useEffect(() => {
+        const getapi = async () => {
+            try {
+
+                const dataresponse = await axios.get(apiurl + "/api/setting/allowlink");
+                const data = dataresponse.data;
+                setData(data);
+            } catch (err) {
+                console.log(err);
+                setErrormsg(err.response.data.msg);
+            }
+        }
+        getapi();
+    }, [])
     return (
         <div className=" overflow-x-auto shadow-2xl">
             <table className="border-separate w-full">
@@ -12,21 +44,25 @@ export default function UrlboxIcon() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>
-                            /userinfo
-                        </td>
-                        <td>
-                            ข้อมูลผู้ใช้งาน
-                        </td>
-                        <td>
-                            <select>
-                                <option>
-                                    ---
-                                </option>
-                            </select>
-                        </td>
-                    </tr>
+                    {data.length > 0 && data.filter(v =>v.icon !== null).map((v, i) => (
+                        v.icon !== undefined && (
+                            <tr key={i}>
+                                <td>
+                                    {v.linkpath}
+                                </td>
+                                <td>
+                                    {v.pathname}
+                                </td>
+                                <td>
+                                    <MySelect data={listicon} value={parseInt(v.icon)} onChange={(v) => {
+                                        // ทำสิ่งที่ต้องการเมื่อมีการเปลี่ยนแปลงค่า
+                                    }} />
+                                </td>
+                            </tr>
+                        )
+
+                    ))}
+
                 </tbody>
             </table>
         </div>
