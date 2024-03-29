42f2bf39b7d52211d59226d1cc2d153060b5d0ba
KAOOAT <revoitz158@gmail.com>
2024-03-29 20:22:35 +0700

    แก้นิดหน่อย

--- src/pages/ed-pages/regresults_ed.js
+++ src/pages/ed-pages/regresults_ed.js
@@ -205,7 +205,7 @@ const RegResultED = () => {
                 </p>
               ) : subjectssuccess ? (
                 subjectssuccess.length > 0 ? (
-                  <div className="flex flex-1 bg-slate-200 rounded-lg overflow-x-auto shadow-xl h-full overflow-y-auto">
+                  <div className="flex bg-slate-200 rounded-lg overflow-x-auto shadow-xl h-full overflow-y-auto">
                     <table className=" w-full">
                       <thead>
                         <tr className="column-color1 text-white">
@@ -232,11 +232,11 @@ const RegResultED = () => {
 
                       <tbody>
                         {currentsubjects.map((value, index) => (
-                          <tr key={index} className="text-nowrap">
+                          <tr key={index}>
                             <td className="py-2 font-light text-lg text-center">
                               {startIndex + index + 1}
                             </td>
-                            <td className="py-2 font-light text-lg text-center">
+                            <td className="py-2 font-light text-lg text-center text-nowrap">
                               {value.idSubject}-{value.ySubject.substring(2)}
                             </td>
                             <td className="py-2 font-light text-lg text-center">
@@ -254,7 +254,7 @@ const RegResultED = () => {
                             <td className="py-2 font-light text-lg text-center">
                               {value.N_people}
                             </td>
-                            <td className="py-2 font-light text-lg  justify-center flex gap-1 hover:text-green-600 transition-all underline  cursor-pointer">
+                            <td className="py-2 font-light text-lg text-nowrap justify-center flex gap-1 hover:text-green-600 transition-all underline  cursor-pointer">
                               <div
                                 onClick={() => {
                                   let stringdata = "";
